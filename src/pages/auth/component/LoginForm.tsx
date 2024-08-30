df4e50a03388dd8953892cf6525ccb096c21c60f
luthfi070 <luthfimusafa@gmail.com>
2024-08-30 22:04:57 +0700

    redesign login form

--- src/pages/auth/component/LoginForm.tsx
+++ src/pages/auth/component/LoginForm.tsx
@@ -19,36 +19,24 @@ const LoginForm = () => {
   }
 
   return (
-    <Card
-      sx={{
-        border: "0.5px solid #ccc",
-        boxShadow: 3,
-        borderRadius: 2,
-        transition: "0.3s",
-        "&:hover": {
-          boxShadow: 6,
-        },
-      }}
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <InputGroup
-          label="Nama Pengguna"
-          placeholder="masukan nama pengguna anda disini"
-          {...register("username")}
-          required
-        />
-        <InputGroup
-          label="Kata Sandi"
-          type="password"
-          placeholder="masukan kata sandi anda disini"
-          {...register("password")}
-          required
-        />
-        <Button type="submit" sx={{ mt: 2 }}>
-          Masuk
-        </Button>
-      </form>
-    </Card>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <InputGroup
+        label="Nama Pengguna"
+        placeholder="masukan nama pengguna anda disini"
+        {...register("username")}
+        required
+      />
+      <InputGroup
+        label="Kata Sandi"
+        type="password"
+        placeholder="masukan kata sandi anda disini"
+        {...register("password")}
+        required
+      />
+      <Button type="submit" sx={{ mt: 2 }}>
+        Masuk
+      </Button>
+    </form>
   )
 }
 
