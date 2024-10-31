1ec25a19a19fd044abe69a80918254505dec1731
kriptonhaz <kriptonhaz@gmail.com>
2024-10-31 10:49:39 +0700

    add card timer

---
+++ src/pages/lembar-ujian/component/cardTimer.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { Box, Card, CardContent, Typography, Button } from "@mui/material"
+import { TextIncrease, TextDecrease } from "@mui/icons-material"
+import { formatTime } from "@/utils/timer"
+
+const CardTimer = ({
+  remainingTime,
+  timerType,
+  subtestNumber,
+  subtestName,
+  onIncreaseFont,
+  onDecreaseFont,
+}: {
+  remainingTime: number
+  timerType: number
+  subtestNumber?: string
+  subtestName?: string
+  onIncreaseFont: () => void
+  onDecreaseFont: () => void
+}) => {
+  return (
+    <Card
+      sx={{
+        width: "98%",
+        border: "0.5px solid #ccc",
+        boxShadow: 3,
+        borderRadius: 2,
+        transition: "0.3s",
+        "&:hover": {
+          boxShadow: 6,
+        },
+        mb: 5,
+        position: "sticky",
+        top: "80px",
+        zIndex: 9999,
+      }}
+    >
+      <CardContent sx={{ padding: 0, paddingBottom: "0px !important" }}>
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <Typography variant="h6">Sisa Waktu: {formatTime(remainingTime)}</Typography>
+          <Box sx={timerType === 2 ? { width: "250px", display: "flex", justifyContent: "space-between" } : {}}>
+            <Box display={"flex"} justifyContent={"space-between"} width={140}>
+              <Button color="primary" startIcon={<TextDecrease />} variant="outlined" onClick={onDecreaseFont} />
+              <Button color="primary" startIcon={<TextIncrease />} variant="outlined" onClick={onIncreaseFont} />
+            </Box>
+          </Box>
+        </Box>
+        <Typography variant="h6" sx={{ mb: 3 }}>
+          {subtestNumber} : {subtestName}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default CardTimer
