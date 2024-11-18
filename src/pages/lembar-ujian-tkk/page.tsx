2d183c738e0957cc415c4cd86af787c05d14279c
kriptonhaz <kriptonhaz@gmail.com>
2024-11-18 16:33:55 +0700

    fix note

--- src/pages/lembar-ujian-tkk/page.tsx
+++ src/pages/lembar-ujian-tkk/page.tsx
@@ -175,11 +175,11 @@ const LembarUjianTkk: React.FC = () => {
           setIndexSubtestActiveTkk(0)
           setCurrentQuestionIndex(0)
         } else {
-          // checkQuestionAvailable()
+          checkQuestionAvailable()
 
           // NOTE: only for testing
-          setIndexSubtestActiveTkk(2)
-          setCurrentQuestionIndex(0)
+          // setIndexSubtestActiveTkk(0)
+          // setCurrentQuestionIndex(0)
         }
       }
       checkActivity()
