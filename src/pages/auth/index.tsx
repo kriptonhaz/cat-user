6bdb28ce3e447d816bc7347c4384b86bb3d52c91
Abdul Latif Mubasir <bj.angel119@gmail.com>
2025-01-10 10:30:48 +0700

    fix: image slider login form

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -4,6 +4,10 @@ import { Splide, SplideSlide } from "@splidejs/react-splide"
 import Logo from "@/assets/logo-kemenhan.png"
 import { Box, Container, Grid, Stack, Typography } from "@mui/material"
 import { primary } from "@/theme/ts/colors"
+import Thumbnail1 from "@/assets/slider/image-1.jpg"
+import Thumbnail2 from "@/assets/slider/image-2.jpg"
+import Thumbnail3 from "@/assets/slider/image-3.jpg"
+import Thumbnail4 from "@/assets/slider/image-4.jpg"
 
 const LoginPage: React.FC = () => {
   return (
@@ -24,32 +28,48 @@ const LoginPage: React.FC = () => {
         >
           <Splide aria-label="My Favorite Images">
             <SplideSlide>
-              <img
-                src="https://images.unsplash.com/photo-1534644107580-3a4dbd494a95?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Image 1"
-                style={{ width: "100%" }}
-              />
+              <div
+                style={{
+                  backgroundImage: `url(${Thumbnail1})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                  height: "100vh",
+                }}
+              ></div>
             </SplideSlide>
             <SplideSlide>
-              <img
-                src="https://images.unsplash.com/photo-1503676382389-4809596d5290?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Image 2"
-                style={{ width: "100%" }}
-              />
+              <div
+                style={{
+                  backgroundImage: `url(${Thumbnail2})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                  height: "100vh",
+                }}
+              ></div>
             </SplideSlide>
             <SplideSlide>
-              <img
-                src="https://images.unsplash.com/photo-1570616969692-54d6ba3d0397?q=80&w=2922&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Image 3"
-                style={{ width: "100%" }}
-              />
+              <div
+                style={{
+                  backgroundImage: `url(${Thumbnail3})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                  height: "100vh",
+                }}
+              ></div>
             </SplideSlide>
             <SplideSlide>
-              <img
-                src="https://images.unsplash.com/photo-1565689157206-0fddef7589a2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Image 4"
-                style={{ width: "100%" }}
-              />
+              <div
+                style={{
+                  backgroundImage: `url(${Thumbnail4})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                  height: "100vh",
+                }}
+              ></div>
             </SplideSlide>
           </Splide>
         </Grid>
